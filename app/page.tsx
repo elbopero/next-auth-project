69f0bcc598c3f6a075a1d5ecc86d324621095b15
Aman <amanboparai@live.no>
2024-02-11 20:15:47 +0100

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="font-serif font-bold text-green-500">Hey</p>
       <Button variant="default" size="lg">Click Me</Button>
     </div>
   );
