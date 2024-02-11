170260ee7cf49990df6463a2a3ce12fccca0f791
Aman <amanboparai@live.no>
2024-02-11 21:07:58 +0100

    tested routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  console.log('where are we?');
   return (
     <div>
       <Button variant="default" size="lg">Click Me</Button>
